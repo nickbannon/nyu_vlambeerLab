97e8e64747c530d3884720b46ea809f8a518a71c
Unknown <njb350@nyu.edu>
2017-10-31 14:02:29 -0400

    10.31
    final, lookin gooood

---
+++ Assets/scripts/GameControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+
+	bool gameOver;
+	public GameObject gameOverObj;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameOver){
+			if (Pathmaker.currentTiles == Pathmaker.maxTiles) {
+				gameOver = true;
+				gameOverObj.SetActive(true);
+				Debug.Log("over");
+			}
+		}
+		else{
+			if (Input.GetKeyDown(KeyCode.R)) {
+				Pathmaker.currentTiles = 0;
+				Pathmaker.tiles = new List<GameObject>();
+				SceneManager.LoadScene("mainLabScene");
+			}
+		}
+	}
+}
